ccd1b8f936751cea83163a915f7c6d10b46eb091
AndyL <missedmemo@gmail.com>
2019-06-28 10:01:49 -0700

    add basic package management

--- 2-components/src/app.js
+++ 2-components/src/app.js
@@ -1,36 +1,38 @@
-
 const Pet = ({ name, animal, breed }) => {
-  return React.createElement( "div", {},
-    React.createElement( "h1", {}, name ),
-    React.createElement( "h2", {}, animal ),
-    React.createElement( "h2", {}, breed )
-  )
-}
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  );
+};
 
 const App = () => {
-  return React.createElement( "div", {},
-    React.createElement( "h1", {},
+  return React.createElement(
+    "div",
+    {},
+    React.createElement(
+      "h1",
+      {},
       "Adopt Us, You Heartless Bastards!",
-      React.createElement( Pet, {
+      React.createElement(Pet, {
         name: "Fifi",
         animal: "Dog",
         breed: "Poodle"
       }),
-      React.createElement( Pet, {
+      React.createElement(Pet, {
         name: "Bonzo",
         animal: "Dog",
         breed: "Boxer"
       }),
-      React.createElement( Pet, {
+      React.createElement(Pet, {
         name: "Fritz",
         animal: "Dog",
         breed: "German Shepherd"
       })
     )
-  )
-}
+  );
+};
 
-ReactDOM.render(
-  React.createElement(App),
-  document.getElementById('root')
-)
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
