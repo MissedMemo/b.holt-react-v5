485566908c908c9aecc7bd588cdbfff661eb4dcb
AndyL <missedmemo@gmail.com>
2019-07-02 15:35:32 -0700

    begin search params component

--- 3-jsx-hooks/src/app.js
+++ 3-jsx-hooks/src/app.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
-import { Pet } from "./pet.jsx";
+import SearchParams from "./search-params.jsx";
 
 const App = () => (
   <div>
     <h1>Adopt Us, You Heartless Bastards!</h1>
-    <Pet name="Fifi" animal="Dog" breed="Poodle" />
-    <Pet name="Polly" animal="Bird" breed="Parrot" />
-    <Pet name="Fluff" animal="Cat" breed="Siamese" />
+    <SearchParams />
   </div>
 );
 
