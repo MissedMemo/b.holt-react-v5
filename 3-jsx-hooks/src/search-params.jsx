19cf9d147cdecaaa17026b282fd4c99f1d9878e1
AndyL <missedmemo@gmail.com>
2019-07-03 13:20:04 -0700

    apply state hook pattern

--- 3-jsx-hooks/src/search-params.jsx
+++ 3-jsx-hooks/src/search-params.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  const [location, setLocation] = useState("Seattle, WA");
+
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="location" />
+          <input
+            id="location"
+            value={location}
+            onChange={e => setLocation(e.target.value)}
+            placeholder="location"
+          />
         </label>
         <button>Submit</button>
       </form>
