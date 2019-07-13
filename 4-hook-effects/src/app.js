4d0668f06c8a18aef7713d761e187d24467266e4
AndyL <missedmemo@gmail.com>
2019-07-12 22:18:31 -0700

    add basic routing

--- 4-hook-effects/src/app.js
+++ 4-hook-effects/src/app.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
 import SearchParams from "./search-params.jsx";
+import Details from "./details.jsx";
 
 const App = () => (
   <React.StrictMode>
     <div>
-      <h1>Adopt Us, You Heartless Bastards!</h1>
-      <SearchParams />
+      <header>
+        <Link to="/">Adopt Us, You Heartless Bastards!</Link>
+      </header>
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </div>
   </React.StrictMode>
 );
