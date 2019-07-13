0fc0887f8c55c8afc8feca88f181273b5a17cd77
AndyL <missedmemo@gmail.com>
2019-07-12 20:52:29 -0700

    refactor layout of pets query results

--- 4-hook-effects/src/pet.jsx
+++ 4-hook-effects/src/pet.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 
-const Pet = ({ name, animal, breed }) => (
-  <div>
-    <h1>{name}</h1>
-    <h2>{animal}</h2>
-    <h2>{breed}</h2>
-  </div>
-);
+const Pet = ({ name, animal, breed, media, location, id }) => {
+  const hero = media.length ? media[0].small : "http://placecorgi.com/300/300";
+
+  return (
+    <a href="/details/${id}" className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
+  );
+};
 
 export default Pet;
