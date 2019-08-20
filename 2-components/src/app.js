65af1652631ce6d68dbf5de9ec1089cfca6c9870
AndyL <missedmemo@gmail.com>
2019-08-19 21:53:16 -0700

    segregate into components

---
+++ 2-components/src/app.js
@@ -0,0 +1,21 @@
+
+const Pet = ({ name, animal, breed }) =>
+  React.createElement( "div", {}, [
+    React.createElement( "h1", {}, name ),
+    React.createElement( "h2", {}, animal ),
+    React.createElement( "h2", {}, breed )
+  ])
+
+const App = () =>
+  React.createElement( "div", {}, [
+    React.createElement( "a", { class: "logo" } ),
+    React.createElement( "h1", {}, "Let's adopt a cute pet..." ),
+    React.createElement( Pet, { name: "Fido", animal: "dog", breed: "Pomeranian"} ),
+    React.createElement( Pet, { name: "Fluff", animal: "cat", breed: "Siamese"} ),
+    React.createElement( Pet, { name: "Squawk", animal: "bird", breed: "Parrot"} )
+  ])
+
+ReactDOM.render(
+  React.createElement(App),
+  document.getElementById("root")
+)
