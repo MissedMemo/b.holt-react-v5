04a5be573ab0c41cf4ccb5cbb7123187798af076
AndyL <missedmemo@gmail.com>
2019-08-20 22:49:06 -0700

    add basic React Hooks plugins etc. support

--- 3-jsx-hooks/src/app.js
+++ 3-jsx-hooks/src/app.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./pet";
+import SearchParams from "./search-params";
 
 const App = () => (
   <div>
     <div className="logo" />
     <h1>Let&apos;s adopt a pet!</h1>
-    <Pet name="Fido" animal="dog" breed="Pomeranian" />
-    <Pet name="Fluffy" animal="cat" breed="Siamese" />
-    <Pet name="Eduardo" animal="bird" breed="Parrot" />
+    <SearchParams />
   </div>
 );
 
