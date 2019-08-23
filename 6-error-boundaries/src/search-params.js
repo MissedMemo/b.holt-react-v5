c5ec8545f39347fd0d4d2779b71588d40e895a4e
AndyL <missedmemo@gmail.com>
2019-08-22 22:33:33 -0700

    sub-project to clear up error boundary issues

---
+++ 6-error-boundaries/src/search-params.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
+import Results from "./results";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropDown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropDown, setBreed] = useDropdown("Breed", "", breeds);
+  const [pets, setPets] = useState([]);
+
+  const fetchPets = async () => {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+
+    setPets(animals || []);
+  };
+
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    // note 'breeds' rename to differentiate from useState variable
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      const breedStrings = apiBreeds.map(({ name }) => name);
+      setBreeds(breedStrings);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
+
+  return (
+    <div className="search-params">
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          fetchPets();
+        }}
+      >
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="locaction"
+            onChange={e => setLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropDown />
+        <BreedDropDown />
+        <button>Submit</button>
+      </form>
+      <Results pets={pets} />
+    </div>
+  );
+};
+
+export default SearchParams;
