d17659841c8aab6911f7b5424dbbe4dd337c0192
AndyL <missedmemo@gmail.com>
2019-08-25 15:18:49 -0700

    define simple style guide

--- 9-css-in-js/src/navbar.js
+++ 9-css-in-js/src/navbar.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { Link } from "@reach/router";
+import Color from "./colors";
 import { css } from "@emotion/core";
 
 const NavBar = () => (
   <header
     css={css`
-      background-color: #333;
+      background-color: ${Color.primary};
       padding: 15px;
     `}
   >
     <Link to="/" className="logo">
       This text ignored... How RUDE!
     </Link>
-    <span role="img" aria-label="logo">
+    <span
+      role="img"
+      aria-label="logo"
+      css={css`
+        font-size: 40px;
+        &:hover {
+          font-size: 60px;
+        }
+      `}
+    >
       🐨🦁🐹
     </span>
   </header>
